a3b35ac4d91ab5f6bb1101284063c75324f31e36
GuyMiz95 <guy.mizrahi95@gmail.com>
2025-03-30 17:02:28 +0300

    Initial commit - Project setup

---
+++ src/main/java/com/example/ticketbookingsystem/PopcornPalaceTicketBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.ticketbookingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopcornPalaceTicketBookingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PopcornPalaceTicketBookingSystemApplication.class, args);
+    }
+
+}
